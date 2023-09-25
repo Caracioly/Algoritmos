a166eb081bc599aab7ccb37950ed2d6f10454711
André Caracioly <andrecaracioly@gmail.com>
2023-09-25 13:26:53 -0300

    dark-mode img icon

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function darkMode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-    var button = document.querySelector(".dark-mode-toggle");
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+  var image = document.getElementById("image-btn");
+
   if (element.classList.contains("dark-mode")) {
-    button.textContent = "Modo Claro";
+    image.src = "to-light.png";
   } else {
-    button.textContent = "Modo Escuro";
+    image.src = "to-dark.png";
   }
   }
